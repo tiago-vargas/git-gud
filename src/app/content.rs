8da0bc124fec1e23b9d0d0e508cfbf8e6f926d99
Tiago Vargas <tiago.vargaspo@proton.me>
2024-06-01 20:49:00 -0300

    Select repository from file dialog
    Shows a file chooser dialog. It only selects folders.

--- src/app/content.rs
+++ src/app/content.rs
@@ -1,14 +1,22 @@
-use gtk::prelude::*;
+use gtk::{gio, prelude::*};
 use relm4::prelude::*;
 
-pub(crate) struct Model;
+pub(crate) struct Model {
+	selected_repository: Option<gio::File>,
+}
 
 pub(crate) struct Init;
+
+#[derive(Debug)]
+pub(crate) enum Input {
+	ShowOpenRepoDialog,
+	PlaceholderAction(Option<gio::File>),
+}
 
 #[relm4::component(pub(crate))]
 impl SimpleComponent for Model {
 	type Init = Init;
-	type Input = ();
+	type Input = Input;
 	type Output = ();
 
 	view! {
@@ -26,6 +34,10 @@ impl SimpleComponent for Model {
 					set_label: "Open Repository",
 					add_css_class: "suggested-action",
 					add_css_class: "pill",
+
+					connect_clicked[sender] => move |_| {
+						sender.input(Self::Input::ShowOpenRepoDialog)
+					},
 				},
 			},
 		}
@@ -34,16 +46,55 @@ impl SimpleComponent for Model {
 	fn init(
 		_init: Self::Init,
 		root: Self::Root,
-		_sender: ComponentSender<Self>,
+		sender: ComponentSender<Self>,
 	) -> ComponentParts<Self> {
-		let model = Self;
+		let model = Self {
+			selected_repository: None,
+		};
 
 		let widgets = view_output!();
 
 		ComponentParts { model, widgets }
 	}
 
-	fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
-		let () = message;
+	fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
+		match message {
+			Self::Input::ShowOpenRepoDialog => {
+				let app = relm4::main_application();
+				let main_window = app
+					.windows()
+					.first()
+					.expect(
+						"Event should have been triggered by last focused window, thus first item",
+					)
+					.clone();
+
+				// Use `gtk::FileDialog` instead when Relm4 ship with a new GTK version.
+				// `gtk::FileChooserDialog` is deprecated from 4.10 onwards, but we ain't there yet.
+				let dialog = gtk::FileChooserDialog::builder()
+					.title("Open Repository")
+					.transient_for(&main_window)
+					.action(gtk::FileChooserAction::SelectFolder)
+					.modal(true)
+					.build();
+				dialog.add_button("Open", gtk::ResponseType::Accept);
+				dialog.add_button("Cancel", gtk::ResponseType::Cancel);
+				dialog.connect_response(move |this, response| match response {
+					gtk::ResponseType::Accept => {
+						let selected_folder = this.file();
+						sender.input(Self::Input::PlaceholderAction(selected_folder));
+						this.close();
+					}
+					gtk::ResponseType::Cancel => this.close(),
+					gtk::ResponseType::DeleteEvent => (),
+					other => unimplemented!("Unexpected response type: {other:?}"),
+				});
+
+				dialog.present();
+			}
+			Self::Input::PlaceholderAction(repository) => {
+				self.selected_repository = repository;
+			}
+		}
 	}
 }
